aead1ca52e60dd0d29c3d1739504864c1fde82b7
Misbah Fatma <misbah7370@gmail.com>
2023-11-28 21:09:52 +0530

    first commit

---
+++ src/pages/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+                 
+                         
+                     <span class="center"> &copy; Made by MISBAH FATMA </span>         
+              
+                                                                                     
+
+	<p></p>
+            <a className="text-dark fs-4" href="https://github.com/misbah-fatma" target="_blank" rel="noreferrer">
+            <FaGithub size={30} />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
