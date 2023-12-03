a54b32cdb29167b615bcb0b75b10f5a7dc924aed
Misbah Fatma <misbah7370@gmail.com>
2023-12-03 11:47:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <div className="blur" style={{top: '-18%', right: '0'}}></div>
         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
         <Home/>
-     
         <Auth/>
         <Footer/>
     </div>
--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -5,14 +5,10 @@ const Footer = () => {
   return (
     <>
       <footer className="mb-0 text-center">
-        <div className="d-flex align-items-center justify-content-center pb-5">
-          <div className="col-md-6">
-                 
-                         
-                     <span class="center"> &copy; Made by MISBAH FATMA </span>         
-              
-                                                                                     
-
+        <div className="d-flex align-items-center justify-content-center ">
+          <div className="col-md-3">        
+          <span class="center"> &copy; Made by MISBAH FATMA </span>         
+                                                                                           
 	<p></p>
             <a className="text-dark fs-4" href="https://github.com/misbah-fatma" target="_blank" rel="noreferrer">
             <FaGithub size={30} />
